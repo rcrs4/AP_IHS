f434b1ab86417730b9e86803269cf21739c062e8
Rafael Carneiro Reis de Souza <rcrs4@ug3c40.windows.cin.ufpe.br>
2019-04-03 13:05:26 -0300

    Apenas nenhuma avanço

--- AP1.asm
+++ AP1.asm
@@ -7,7 +7,7 @@ editarc db 'Editar Conta: 3',0dh,0x0a,0
 deletarc db 'Deletar Conta: 4',0dh,0x0a,0
 listara db 'Listar Agencia: 5',0dh,0x0a,0
 listarcontasa db 'Listar Contas de uma Agencia: 6',0dh,0x0a,0
-Cadastros db 0
+CadastrarNome db 'Coloque seu Nome', 0dh,0x0a, 0
 readCharN:
 	mov ah, 0h
 	int 16h
@@ -26,24 +26,34 @@ readCharN:
 ret
 
 cadastro:
+	xor cx, cx
+	push si
 	call readCharN
-	lodsb
-	start_cadastro:
-	cmp al, 0
-	mov byte[Cadastros], al
-	je end_cadastro
-	jmp start_cadastro
+	nome:
+		lodsb
+		cmp al, 0
+		je end_cadastro
+		mov byte[si], al
+		inc cx
+		jmp nome
 	end_cadastro:
+	mov byte[si], al
+	mov dx, 20
+	sub dx, cx
+	add si, dx
+	pop si
 ret
 
 ler:
-	mov al, byte[Cadastros]
+	mov al, byte[bx]
 	cmp al, 0
 	je end_ler
 	mov ah, 0xe
 	int 10h
 	jmp ler
 	end_ler:
+	mov ah, 0xe
+	int 10h
 ret
 
 puts:
@@ -63,10 +73,15 @@ ret
 veriComando:
 	call readCharN
 	lodsb
-	inc si
-	cmp al, "1"
-	jne end_veri
-	call cadastro
+	start_comando:
+		inc si
+		cmp al, "1"
+		jne end_veri
+		push si
+		mov si, CadastrarNome
+		call puts
+		pop si
+		call cadastro
 	end_veri:
 ret
 
